64f817b790f1bce8c1781818faa9f085375ae39a
sdwdjzhy@qq.com <sdwdjzhy>
2021-01-30 17:18:32 +0800

    添加项目文件。

---
+++ TestMultipleController/ControllerNamespaceConstraintAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing;
+
+namespace TestMultipleController
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class ControllerNamespaceConstraintAttribute : Attribute, IActionConstraint
+    {
+        public int Order => 1;
+        /// <inheritdoc />
+        public bool Accept(ActionConstraintContext context)
+        {
+            return IsValidForRequest(context.RouteContext, context.CurrentCandidate.Action);
+        }
+        public bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+        {
+            var actionNamespace = ((ControllerActionDescriptor)action).MethodInfo.DeclaringType.Namespace;
+            Console.WriteLine("IsValidForRequest:" + actionNamespace);
+            Console.WriteLine("routeContext.RouteData.DataTokens:" + routeContext.RouteData.DataTokens.Count());
+            if (routeContext.RouteData.DataTokens.ContainsKey("Namespace"))
+            {
+                var dataTokenNamespace = (string)routeContext.RouteData.DataTokens.FirstOrDefault(dt => dt.Key == "Namespace").Value;
+                return dataTokenNamespace == actionNamespace;
+            }
+            return true;
+        }
+    }
+}
---
+++ TestMultipleController/Controllers/HomeController - 复制.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestMultipleController;
+
+namespace Test.Controllers
+{
+    [ControllerNamespaceConstraint]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content(RouteData.DataTokens.Count.ToString());
+        }
+    }
+}
