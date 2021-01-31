ef1738f117ebd210d5b362f14d5b31781b574802
sdwdjzhy <sdwdjzhy@qq.com>
2021-01-31 20:44:08 +0800

    根据 https://stackoverflow.com/questions/65966439/how-can-i-find-controller-with-special-namespace-in-asp-net-core 完成对不同类库及命名空间的同名Controller的覆盖

---
+++ TestMultipleControllerWeb/ActionModelMatchingEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace TestMultipleControllerWeb
+{
+    public class ActionModelMatchingEqualityComparer : IEqualityComparer<ActionModel>
+    {
+        static readonly Lazy<ActionModelMatchingEqualityComparer> _instanceLazy =
+            new Lazy<ActionModelMatchingEqualityComparer>(() => new ActionModelMatchingEqualityComparer());
+        public static ActionModelMatchingEqualityComparer Instance => _instanceLazy.Value;
+        public bool Equals(ActionModel x, ActionModel y)
+        {
+            return string.Equals(x.ActionName, y.ActionName, StringComparison.OrdinalIgnoreCase) &&
+                   x.Attributes.OfType<IActionHttpMethodProvider>().SelectMany(e => e.HttpMethods).DefaultIfEmpty("GET")
+                    .Intersect(y.Attributes.OfType<IActionHttpMethodProvider>().SelectMany(e => e.HttpMethods).DefaultIfEmpty("GET"),
+                               StringComparer.OrdinalIgnoreCase).Any();
+
+        }
+
+        public int GetHashCode(ActionModel obj)
+        {
+            return (obj.ActionName?.GetHashCode() ?? 0) ^
+                    obj.Attributes.OfType<IActionHttpMethodProvider>()
+                       .SelectMany(e => e.HttpMethods).Distinct(StringComparer.OrdinalIgnoreCase)
+                       .OrderBy(e => e).Aggregate(0, (c, e) => (c * 13) ^ e.GetHashCode());
+        }
+    }
+}
---
+++ TestMultipleControllerWeb/DuplicateActionsRemovalApplicationModelConvention.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace TestMultipleControllerWeb
+{
+    public class DuplicateActionsRemovalApplicationModelConvention : IApplicationModelConvention
+    {
+        public void Apply(ApplicationModel application)
+        {
+            Console.WriteLine("DuplicateActionsRemovalApplicationModelConvention");
+            var currentExecutingAsm = Assembly.GetExecutingAssembly();
+            var allControllers = application.Controllers;
+
+            var overriddenActions = allControllers.GroupBy(e => e.ControllerName)
+                                               //select only duplicate controllers
+                                               .Where(g => g.Count() > 1)
+                                               .SelectMany(g => g.SelectMany(e => e.Actions)
+                                                                 .GroupBy(o => o, ActionModelMatchingEqualityComparer.Instance)
+                                                                 //select only duplicate actions
+                                                                 .Where(k => k.Count() > 1)
+                                                                 .SelectMany(e => e.OrderByDescending(x => x.Controller.ControllerType.Assembly == currentExecutingAsm)
+                                                                     //select all except the action defined in the current executing assembly
+                                                                     .Skip(1)))
+                                               .Select(e => (controller: e.Controller, action: e));
+            //for each overridden action, just remove it from the owner controller
+            //this effectively means that the removed action will be overridden
+            //by the only remaining one (defined in the current executing assembly)
+            foreach (var overriddenAction in overriddenActions)
+            {
+                overriddenAction.controller.Actions.Remove(overriddenAction.action);
+            }
+        }
+    }
+}
